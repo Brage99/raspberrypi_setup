289e0468eb318327b6e7534707b616d775933025
Brage99 <125267521+Brage99@users.noreply.github.com>
2024-09-23 16:35:21 +0200

    Update gps_test.py

--- LCD/LCD/gps_test.py
+++ LCD/LCD/gps_test.py
@@ -1,6 +1,8 @@
 import serial
 import time
 import pynmea2
+import RPi.GPIO as GPIO
+from bmp180 import *
 
 port = '/dev/ttyS0'
 baud = 9600
@@ -17,8 +19,12 @@ while True:
         msg = None
         try:
             msg = pynmea2.parse(str)
-            strMsg = "Timestamp: %s,Lat: %s Lon: %s,Altitude: %s %s,Satellites: %s" % (msg.timestamp,ro>
+            strMsg = "Timestamp: %s,Lat: %s Lon: %s,Altitude: %s %s,Satellites: %s" % (msg.timestamp,round(msg.latitude,6),round(msg.longitude,6),msg.altitude,msg.altitude_units,msg.num_sats)
             print(strMsg)
+            temperature, pressure = readBmp180()
+            print(f"Temperature: {temperature} C") 
+            print(f"Pressure: {pressure} Pa") 
+            print()
         except Exception as e:
             print(e)
     time.sleep(0.01)
